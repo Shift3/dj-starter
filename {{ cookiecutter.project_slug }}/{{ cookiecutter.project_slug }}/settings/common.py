4e3bcf9e273c46869de88e1831227d15a72e5263
Justin <AmbientRevolution@gmail.com>
2022-05-10 13:27:07 -0700

    Split requirements (#3)
    Split requirements into their own prod/dev/test separation.

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings/common.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings/common.py
@@ -56,7 +56,6 @@ MIDDLEWARE = (
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
