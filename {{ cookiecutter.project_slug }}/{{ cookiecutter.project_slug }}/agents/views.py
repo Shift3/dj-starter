eb85838e9a7a6316f3e61f737b48818f1060468a
Justin Schiff <AmbientRevolution@gmail.com>
2022-05-18 18:46:16 -0700

    Add ability to use ?search query param on agents and users endpoints

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/agents/views.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/agents/views.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from {{ cookiecutter.project_slug }}.core.filters import CamelCaseDjangoFilterBackend, CamelCaseOrderingFilter
@@ -11,9 +12,15 @@ class AgentViewSet(viewsets.ModelViewSet):
     queryset = Agent.objects.all()
     serializer_class = AgentSerializer
     permission_classes = (IsAuthenticated, IsAnyRole([User.EDITOR, User.USER, User.ADMIN]),)
-    filter_backends = (CamelCaseDjangoFilterBackend, CamelCaseOrderingFilter,)
+    filter_backends = (
+        filters.SearchFilter,
+        CamelCaseOrderingFilter,
+        CamelCaseDjangoFilterBackend,
+    )
     filterset_fields = {
         'email': ['icontains', 'startswith', 'endswith', 'exact'],
         'name': ['icontains', 'startswith', 'endswith', 'exact'],
         'phone_number': ['icontains', 'startswith', 'endswith', 'exact'],
     }
+    search_fields = ['email', 'name', 'phone_number']
+
