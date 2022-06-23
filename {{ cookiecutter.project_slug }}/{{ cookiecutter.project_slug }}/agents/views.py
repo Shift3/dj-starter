36614587bd167cd3ea0f1158084c9c6b68cf3056
Joshwin Greene <joshwin.greene@gmail.com>
2022-06-23 16:50:25 -0700

    (cont.) Supports the frontend branch that fixes the case-sensitive listview issue (#13)
    Changes
    - Changes startswith and endswith to their case-insensitive versions in the user/agent views

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/agents/views.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/agents/views.py
@@ -26,9 +26,9 @@ class AgentViewSet(viewsets.ModelViewSet):
         CamelCaseDjangoFilterBackend,
     )
     filterset_fields = {
-        "email": ["icontains", "startswith", "endswith", "iexact"],
-        "name": ["icontains", "startswith", "endswith", "iexact"],
-        "phone_number": ["icontains", "startswith", "endswith", "iexact"],
+        "email": ["icontains", "istartswith", "iendswith", "iexact"],
+        "name": ["icontains", "istartswith", "iendswith", "iexact"],
+        "phone_number": ["icontains", "istartswith", "iendswith", "iexact"],
     }
     search_fields = ["email", "name", "phone_number"]
 
